dd603c74f27bb965c310897b572aa16b5a9615f3
ABDELKADER199 <apdoarek22222@gmail.com>
2024-10-11 12:26:02 +0300

    first commit

---
+++ D03/Program.cs
@@ -0,0 +1,52 @@
+﻿
+namespace D03;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		#region Casting Built in Value Types
+		//int x = 50;
+		//long y = 5000;
+
+		////y = x;
+		///// Implicit Casting 
+		///// Int64 = Int32
+		///// Safe Casting , No RunTime Errors
+		//y = long.MaxValue;
+		//x = (int) y;
+  //      ///Int32 = Int64
+  //      ///UnSafe Casting
+  //      ///Explicit Casting
+  //      ///Default behavior for CLR , will not Throw OverflowExceprion when overflow happens
+		
+
+		/////CLR will throw OverFlowException
+		//checked
+		//{
+		//	x = (int)y;
+		//}
+
+
+  //      Console.WriteLine($"X = {x}");
+		//Console.WriteLine($"y = {y}");
+
+	#endregion
+
+		int x = 5;
+		int y = 10;
+		object O1 = new object();
+		object O2 = new object();
+		O1 = x;
+		///Base Ref = Child
+		///Safe
+		///Boxing
+
+		y =(int) O2;
+		///Derived = Base
+		///UnSafe , Explicite
+		///UnBoxing
+
+	}
+
+}
